6c50e74c6cc273dc118d00f383c8dd792688b44c
macuared <danielmacuare@rentalcars.com>
2020-04-16 19:21:23 +0100

    provisioning.sh - Py3.8.2 + Virtualenv ready

--- provisioning.sh
+++ provisioning.sh
@@ -14,14 +14,16 @@
 green_bold="\033[1;32;49m"
 red_bold="\033[1;31;49m"
 normal="\033[0m"
-VENV_NAME="py3e"
+VENV_NAME="venvs/py3e"
 USER_DIR="/home/vagrant/"
 EXPORTS="/vagrant_data/exports"
 PYTHON_VERSION="3.8.2"
+PIP_VERSION="3.8"
 PYTHON_PCKS=(
         build-essential
         zlib1g-dev
-        libncurses5-devlibgdbm-dev
+        libncurses5-dev
+        libgdbm-dev
         libnss3-dev
         libssl-dev
         libreadline-dev
@@ -70,23 +72,36 @@ sudo ./configure --enable-optimizations > /dev/null 2>&1
 sudo make altinstall > /dev/null 2>&1
 
 printf "$green_bold[CLEANUP]\t$normal   - Cleaning:$red_bold Python $PYTHON_VERSION source files$normal\n"
-sudo rm /usr/src/Python-$PYTHON_VERSION.tgz > /dev/null 2>&1 
+sudo rm /usr/src/Python-$PYTHON_VERSION.tgz > /dev/null 2>&1
 
 
 printf "$normal\n\n############################################################################$normal\n"
 printf "$normal\t\t[CONFIGURING] PYTHON $PYTHON_VERSION$normal\n"
 printf "$normal############################################################################$normal\n"
 
+# Enabling aliases expasion on a shell script
+# https://askubuntu.com/questions/98782/how-to-run-an-alias-in-a-shell-script
+shopt -s expand_aliases
 
-# VENV
-printf "$green_bold[SETTING]$normal\t - Setting: $red_bold Aliases at $ALIASES$normal\n"
-source $EXPORTS
+printf "$green_bold[SOURCING]$normal\t  - Sourcing: $red_bold Exports at $EXPORTS$normal\n"
+. $EXPORTS
 
-printf "$green_bold[INSTALLING]$normal\t - Packet: $red_bold Virtualenv $normal\n"
-pip$PIP_VERSION install virtualenv
+printf "$green_bold[ADDING]$normal\t  - Packet: $red_bold Virtualenv to the PATH$normal\n"
+export PATH="$HOME/.local/bin:$PATH"
 
-printf "$green_bold[CREATING]$normal\t\t - Packet: $red_bold Virtualenv Name: $VENV_NAME$normal\n"
-virtualenv -p python$PIP_VERSION $USER_DIR$VENV_NAME/
+printf "$green_bold[INSTALLING]$normal - Packet: $red_bold Virtualenv $normal\n"
+pip$PIP_VERSION install virtualenv > /dev/null 2>&1
+
+printf "$green_bold[CREATING]$normal\t  - Directory for Virtualenv :$red_bold At $USER_DIR$VENV_NAME/$normal\n"
+mkdir -p $USER_DIR$VENV_NAME/
+
+printf "$green_bold[CREATING]$normal\t  - Virtualenv:$red_bold At $USER_DIR$VENV_NAME/$normal\n"
+virtualenv -p python$PIP_VERSION $USER_DIR$VENV_NAME/ > /dev/null 2>&1
 cd $USER_DIR$VENV_NAME/
 source bin/activate
+
+# This needs to install requirements.txt
+printf "$green_bold[INSTALLING]$normal - Packet: $red_bold nornir $normal\n"
+pip$PIP_VERSION install nornir > /dev/null 2>&1
+
 printf "\n\n"
